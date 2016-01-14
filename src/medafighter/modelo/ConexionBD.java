fef84ec1bb07ca22244232200f7feb367b2f526c
FabianEscobar <fabian.escobar@usach.cl>
2016-01-14 02:16:33 -0300

    Esto es lo que trabajé en ConexionBD.
    Estoy teniendo problemas en cómo realizar la consulta y en cómo procesar la respuesta que me dé dicha consulta.
    
    Cuando despierte veré cómo lo hago, aunque si alguien me puede ayudar... :)

--- src/medafighter/modelo/ConexionBD.java
+++ src/medafighter/modelo/ConexionBD.java
@@ -5,19 +5,84 @@
  */
 package medafighter.modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Fabián
  */
 public class ConexionBD {
     
-    public boolean verificarUsuario(String nombreUsuario, String contrasenia){
+    private static final String SERVIDOR = "localhost";
+    private static final String PUERTO = "1527";
+    private static final String NOMBRE_BD = "base";
+    private static final String USUARIO = "nbuser";
+    private static final String PASSWORD = "nbuser";
+    public static final String URL_CONEXION = "jdbc:derby://localhost:1527/contact" + SERVIDOR + ":"+ PUERTO +"/"+ NOMBRE_BD +";user="+ USUARIO +";password=" + PASSWORD;
+
+    private Connection conexion = null;
+    
+    public boolean conectar() throws SQLException {
+        
+        this.conexion = DriverManager.getConnection(URL_CONEXION);
+        
+        if(this.conexion != null) {
+            
+            return true;
+        
+        }
+        
+        else{
+            
+            return false;
+        
+        }
+    
+    }
+    
+    public void desconectar() throws SQLException {
+        
+        if(this.conexion != null) {
+            
+            this.conexion.close();
+        
+        }
+    
+    }
+    
+    public Statement crearConsulta() throws SQLException {
+        
+        if(this.conexion != null) {
+            
+            return this.conexion.createStatement();
+        
+        }
+        
+        else {
+            
+            return null;
+            
+        }
+        
+    }
+    
+    public boolean verificarUsuario(String nombreUsuario, String contrasenia) throws SQLException {
         
         boolean uBoolean = false;
     
+        this.conectar();
+    
+        Statement stmt = this.crearConsulta();
         
-    
-    
+        String consultaNombreUsuario = "SELECT NOMBRE FROM USUARIO";
+        
+        ResultSet rset= stmt.executeQuery(consultaNombreUsuario);
+        
+        
     
         return uBoolean;
     
