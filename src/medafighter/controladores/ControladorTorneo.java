b2c14a0508ac80e9c86fc73c19f51327310bafb6
FabianEscobar <fabian.escobar@usach.cl>
2016-01-18 22:33:25 -0300

    Está lista la verificación de logins del torneo.
    Ahora terminaré la vista medabot para que mañana comencemos la robobatalla.

--- src/medafighter/controladores/ControladorTorneo.java
+++ src/medafighter/controladores/ControladorTorneo.java
@@ -15,6 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import medafighter.modelo.ConexionBD;
+import medafighter.vistas.DialogIngreso;
 import medafighter.vistas.VistaTorneo;
 
 /**
@@ -31,9 +32,13 @@ public class ControladorTorneo implements ActionListener {
     
     private ConexionBD cbd;
     
+    private DialogIngreso di;
+    
     public ControladorTorneo() throws SQLException {
         
-        this.vt = new VistaTorneo(this);        
+        this.vt = new VistaTorneo(this);
+        
+        this.cbd = new ConexionBD();
         
     }
 
@@ -90,13 +95,190 @@ public class ControladorTorneo implements ActionListener {
                 
                 if (validador == true){
                     
-                    String tipoTorneo = (String)this.vt.getTipoTorneo().getSelectedItem();
+                    boolean uBoolean1 = false;
+                    boolean uBoolean2 = false;
+                    boolean uBoolean3 = false;
+                    boolean uBoolean4 = false;
+                    boolean uBoolean5 = false;
+                    boolean uBoolean6 = false;
+                    boolean uBoolean7 = false;
+                    boolean uBoolean8 = false;
                     
-                    this.cc = new ControladorCombate(jugadores, tipoTorneo);
-                       
-                    this.vt.setVisible(false);
+                    String tipoJugador1 = new String();                    
+                    String tipoJugador2 = new String();
+                    String tipoJugador3 = new String();
+                    String tipoJugador4 = new String();
+                    String tipoJugador5 = new String();
+                    String tipoJugador6 = new String();
+                    String tipoJugador7 = new String();
+                    String tipoJugador8 = new String();
+            
+                    try {       
                 
-                    this.vt.dispose();
+                        tipoJugador1 = this.cbd.buscarTipoJugador(jugador1);                
+                        tipoJugador2 = this.cbd.buscarTipoJugador(jugador2);
+                        tipoJugador3 = this.cbd.buscarTipoJugador(jugador3);
+                        tipoJugador4 = this.cbd.buscarTipoJugador(jugador4);
+                        tipoJugador5 = this.cbd.buscarTipoJugador(jugador5);
+                        tipoJugador6 = this.cbd.buscarTipoJugador(jugador6);
+                        tipoJugador7 = this.cbd.buscarTipoJugador(jugador7);
+                        tipoJugador8 = this.cbd.buscarTipoJugador(jugador8);
+                
+                    } 
+            
+                    catch (SQLException ex) {
+                
+                        Logger.getLogger(ControladorTorneo.class.getName()).log(Level.SEVERE, null, ex);
+            
+                    }
+                    
+                    try {
+                
+                        uBoolean1 = this.cbd.verificarSesion(jugador1);
+                        uBoolean2 = this.cbd.verificarSesion(jugador2);
+                        uBoolean3 = this.cbd.verificarSesion(jugador3);
+                        uBoolean4 = this.cbd.verificarSesion(jugador4);
+                        uBoolean5 = this.cbd.verificarSesion(jugador5);
+                        uBoolean6 = this.cbd.verificarSesion(jugador6);
+                        uBoolean7 = this.cbd.verificarSesion(jugador7);
+                        uBoolean8 = this.cbd.verificarSesion(jugador8);
+            
+                    } 
+            
+                    catch (SQLException ex) {
+                
+                        Logger.getLogger(ControladorTorneo.class.getName()).log(Level.SEVERE, null, ex);
+            
+                    }
+                    
+                    if(tipoJugador1.equals("HUMANO")) {
+                
+                        if(uBoolean1 == false) {
+                    
+                            di = new DialogIngreso(this.vt,true,jugador1);
+                    
+                        }                
+                
+                    }
+                    
+                    if(tipoJugador2.equals("HUMANO")) {
+                
+                        if(uBoolean2 == false) {
+                    
+                            di = new DialogIngreso(this.vt,true,jugador2);
+                    
+                        }                
+                
+                    }
+                    
+                    if(tipoJugador3.equals("HUMANO")) {
+                
+                        if(uBoolean3 == false) {
+                    
+                            di = new DialogIngreso(this.vt,true,jugador3);
+                    
+                        }                
+                
+                    }
+                    
+                    if(tipoJugador4.equals("HUMANO")) {
+                
+                        if(uBoolean4 == false) {
+                    
+                            di = new DialogIngreso(this.vt,true,jugador4);
+                    
+                        }                
+                
+                    }
+                    
+                    if(tipoJugador5.equals("HUMANO")) {
+                
+                        if(uBoolean5 == false) {
+                    
+                            di = new DialogIngreso(this.vt,true,jugador5);
+                    
+                        }                
+                
+                    }
+                    
+                    if(tipoJugador6.equals("HUMANO")) {
+                
+                        if(uBoolean6 == false) {
+                    
+                            di = new DialogIngreso(this.vt,true,jugador6);
+                    
+                        }                
+                
+                    }
+                    
+                    if(tipoJugador7.equals("HUMANO")) {
+                
+                        if(uBoolean7 == false) {
+                    
+                            di = new DialogIngreso(this.vt,true,jugador7);
+                    
+                        }                
+                
+                    }
+                    
+                    if(tipoJugador8.equals("HUMANO")) {
+                
+                        if(uBoolean8 == false) {
+                    
+                            di = new DialogIngreso(this.vt,true,jugador8);
+                    
+                        }                
+                
+                    }
+                    
+                    try {
+                
+                        uBoolean1 = this.cbd.verificarSesion(jugador1);
+                        uBoolean2 = this.cbd.verificarSesion(jugador2);
+                        uBoolean3 = this.cbd.verificarSesion(jugador3);
+                        uBoolean4 = this.cbd.verificarSesion(jugador4);
+                        uBoolean5 = this.cbd.verificarSesion(jugador5);
+                        uBoolean6 = this.cbd.verificarSesion(jugador6);
+                        uBoolean7 = this.cbd.verificarSesion(jugador7);
+                        uBoolean8 = this.cbd.verificarSesion(jugador8);
+            
+                    } 
+            
+                    catch (SQLException ex) {
+                
+                        Logger.getLogger(ControladorTorneo.class.getName()).log(Level.SEVERE, null, ex);
+            
+                    }
+                    
+                    if(tipoJugador1.equals("CPU")) uBoolean1 = true;
+                    if(tipoJugador2.equals("CPU")) uBoolean2 = true;
+                    if(tipoJugador3.equals("CPU")) uBoolean3 = true;
+                    if(tipoJugador4.equals("CPU")) uBoolean4 = true;
+                    if(tipoJugador5.equals("CPU")) uBoolean5 = true;
+                    if(tipoJugador6.equals("CPU")) uBoolean6 = true;
+                    if(tipoJugador7.equals("CPU")) uBoolean7 = true;
+                    if(tipoJugador8.equals("CPU")) uBoolean8 = true;
+                    
+                    if((uBoolean1 == true)&&(uBoolean2 == true)&&
+                       (uBoolean3 == true)&&(uBoolean4 == true)&&
+                       (uBoolean5 == true)&&(uBoolean6 == true)&&
+                       (uBoolean7 == true)&&(uBoolean8 == true)) {
+                
+                        String tipoTorneo = (String)this.vt.getTipoTorneo().getSelectedItem();
+                    
+                        this.cc = new ControladorCombate(jugadores, tipoTorneo);
+                       
+                        this.vt.setVisible(false);
+                
+                        this.vt.dispose();
+                
+                    }
+            
+                    else {
+                
+                        JOptionPane.showMessageDialog(this.vt, "No ha sido posible iniciar el torneo debido a que no todos los jugadores fueron ingresados. Si desea iniciar un torneo, puede hacer otro intento ingresando a los jugadores que falten.");
+                
+                    }
                     
                 }
                     
